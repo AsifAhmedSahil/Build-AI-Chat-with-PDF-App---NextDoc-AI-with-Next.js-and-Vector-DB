39a323b3b033182cef0b53d5c6c18dccfb342312
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-04-24 23:08:53 +0600

    inplement multer and store file on the server:

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ import express from "express"
 import cors from "cors"
 import multer from "multer"
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, `${uniqueSuffix}-${file.originalname}`)
+    }
+  })
+
 const upload = multer({dest:"uploads/"})
 
 
